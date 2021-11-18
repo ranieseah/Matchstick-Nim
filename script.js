ae68edc8bb6ded8aca7f2c2cfcd605cede1906f5
ranie <ranieseah@gmail.com>
2021-11-18 22:26:58 +0800

    font end game play set up
    adds class to div after being clicked. added class applies greyscale filter on matchstick to show its been selected.

--- script.js
+++ script.js
@@ -3,11 +3,36 @@ $(() => {
   const imgAdd =
     "https://atlas-content1-cdn.pixelsquid.com/assets_v2/11/1170343860455348201/jpeg-600/H10.jpg";
 
-  const oneMatch = document.createElement("div");
-  document.querySelector(".row").prepend(oneMatch);
-  oneMatch.setAttribute("class", "match");
-  const pic = document.createElement("img");
-  pic.setAttribute("src", imgAdd);
-  pic.setAttribute("height", "100px");
-  oneMatch.append(pic);
+  function oneMatch() {
+    const oneMatch = document.createElement("div");
+    document.querySelector(".row").prepend(oneMatch);
+    oneMatch.setAttribute("class", "match");
+    const pic = document.createElement("img");
+    pic.setAttribute("src", imgAdd);
+    pic.setAttribute("height", "100px");
+    oneMatch.append(pic);
+  }
+
+  function row(num) {
+    const lineBreak = document.createElement("br");
+    document.querySelector(".row").prepend(lineBreak);
+
+    for (let i = num; i > 0; i--) {
+      oneMatch();
+    }
+  }
+
+  function game(num) {
+    for (let i = num; i > 0; i--) {
+      row(i);
+    }
+  }
+  game(6);
+
+  document.querySelector(".row").addEventListener("click", turnGrey);
+  function turnGrey(e) {
+    console.log("hi");
+    e.target.setAttribute("class", "selected");
+    console.log(e.target);
+  }
 });

c16e85cbdaeffdcebc6b806e6a60b12da91ec496
ranie <ranieseah@gmail.com>
2021-11-23 18:29:52 +0800

    made it look pretty, after one player loses, start button refreshes the page.

--- script.js
+++ script.js
@@ -59,6 +59,26 @@ $(() => {
         (this.current = current),
         (this.last = last);
     }
+    switchPlayer() {
+      playerTurn = this.opponent;
+      switch (playerTurn) {
+        case "Player 1":
+          document.querySelector("body").setAttribute("class", "player1");
+          document.querySelector("#start").setAttribute("class", "player1");
+          document.querySelector("#next").setAttribute("class", "player1");
+          document
+            .querySelector("h1")
+            .setAttribute("style", "text-align: left");
+          break;
+        case "Player 2":
+          document.querySelector("body").setAttribute("class", "player2");
+          document.querySelector("#start").setAttribute("class", "player2");
+          document.querySelector("#next").setAttribute("class", "player2");
+          document
+            .querySelector("h1")
+            .setAttribute("style", "text-align: right");
+      }
+    }
     checkStatus() {
       let sum = 0;
       for (let i = 1; i <= Object.keys(gameBoard).length; i++) {
@@ -72,7 +92,14 @@ $(() => {
           this.name,
           "took the last matchstick"
         );
-        document.querySelector("h1").innerText = "LOSER";
+        document.querySelector("h1").innerText = `${this.name} LOST.`;
+        document.querySelector("#start").innerText = "New Game?";
+        document.querySelector("#start").value = "new";
+        document.querySelector("#start").setAttribute("class", "");
+        document
+          .querySelector("#start")
+          .setAttribute("style", "background-color: yellow");
+        document.querySelector("#next").remove();
       } else if (sum === 1) {
         console.log(
           "nice going,",
@@ -81,14 +108,15 @@ $(() => {
         );
         document.querySelector(
           "h1"
-        ).innerText = `${this.name} made a winning move. ${this.opponent} accept your fate and select the last matchstick`;
+        ).innerHTML = `${this.name} made a winning move. <br> ${this.opponent} accept your fate and select the last matchstick.`;
+        this.switchPlayer();
       } else {
         console.log("go on, there are", sum, "matchsticks left");
         document.querySelector(
           "h1"
         ).innerText = `${this.opponent}, make your move`;
-        playerTurn = this.opponent;
         console.log(gameBoard);
+        this.switchPlayer();
       }
     }
     move(row, value) {
@@ -113,11 +141,13 @@ $(() => {
       document.querySelector("#input-box").remove();
       document.querySelector("#player").innerText = "Player 1, make your move.";
       document.querySelector("#start").innerText = "Reset Turn";
+      document.querySelector("#start").setAttribute("class", "player1");
       e.target.value = "reset";
       const btn = document.createElement("button");
       btn.innerText = "Confirm End of Turn";
       btn.setAttribute("id", "next");
-      btn.setAttribute("style", "float:right");
+      btn.setAttribute("class", "player1");
+      // btn.setAttribute("style", "float:right");
       document.querySelector(".row").append(btn);
 
       document.querySelector("#next").addEventListener("click", nextTurn);
@@ -146,15 +176,24 @@ $(() => {
           currentRow = 0;
         }
       }
-    } else {
-      console.log(document.querySelectorAll(".selected"));
-      const toReset = document.querySelectorAll(".selected");
-      for (let item of toReset) {
-        item.setAttribute("class", "unselected");
+    } else if (e.target.value === "reset") {
+      if (currentRow === 0) {
+        alert(
+          "eh? you didnt click anything what. nothing to reset leh. To restart the game, refresh the page :)"
+        );
+      } else {
+        console.log(document.querySelectorAll(".selected"));
+        const toReset = document.querySelectorAll(".selected");
+        for (let item of toReset) {
+          item.setAttribute("class", "unselected");
+        }
+        currentTurn = 0;
+        currentRow = 0;
+        console.log("reset button pressed");
+        alert("ok la - u can pick another row now..");
       }
-      currentTurn = 0;
-      currentRow = 0;
-      console.log("reset button pressed");
+    } else {
+      location.reload();
     }
   }
 
@@ -170,7 +209,7 @@ $(() => {
       ) {
         if (e.target.className === "unselected") {
           if (currentTurn === 3) {
-            alert("Sorry, max moves is 5 matchsticks per round");
+            alert("Sorry, max moves is 3 matchsticks per round");
           } else {
             currentRow = e.target.parentElement.attributes[1].nodeValue;
             turnGrey(e);
